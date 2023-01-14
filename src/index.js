520965718b59fc103d794fea07a9bcfc469f3d0d
RaVi <rafael.raiol@gmail.com>
2023-01-14 02:37:32 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const core    = require('@actions/core')
-const { getBifrostSyncVersionNumber } = require('./business/bifrost')
-const github  = require('@actions/github')
+const core    = require('@actions/core');
+const { getBifrostSyncVersionNumber } = require('./business/bifrost');
+const github  = require('@actions/github');
 
 (async () => {
   await getBifrostSyncVersionNumber(core, github)
